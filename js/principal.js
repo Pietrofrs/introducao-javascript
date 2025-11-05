4787576aba0c27234bf13bd66bb0073f8a0a6d08
Pietrofrs <rodrigues.pietro@escola.pr.gov.br>
2025-11-05 11:30:35 -0300

    peso

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,12 @@
-	var titulo = document.querySelector(".titulo");
-			titulo.textContent = "Aparecida Nutricionista";
-            /* IMC = massa
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+/*  IMC = massa/altura^2    */
+var paciente = paciente document.querySelector("#primeiro-paciente")
+var tdPeso = document.querySelector(".info-peso")
+var peso = tdPeso.textContent;
+
+
+console.log(paciente); //tr
+console.log(paciente); //td que tem peso
+console.log(peso);
